fb7b10f00e3f81a04828377f0df4fa3ccbbd1a14
Saleha-m14 <>
2023-05-18 13:50:11 +0000

    Check if the user added a valid name

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 
 def greet_user(name): # function to greet user
-    print("Welcome!")
-    print(f"Hello {username}, Lets play a game.")
-username = input("Add username: ") # input for username
+    if not username.isalpha():
+        # I need to return back here and check username
+        print("Name cannot be a number.")
+    elif len(username) < 3:
+        print("Name should be at least three letters.")
+    elif len(username) > 30:
+        print("User name cannot be more than 15 letters.")
+    else:
+        print("Welcome!")
+        print(f"Hello {username}, Lets play a game.")
+username = input("Add Name: ") # input for username
 greet_user(username)
 
 
