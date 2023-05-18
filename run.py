363f0dbd9ff857613386af8bbde67da13f357ea0
Saleha Mohammadi <saleha.moh2023@gmail.com>
2023-05-18 12:37:38 +0000

    Innitial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
+
+def greet_user(name): # function to greet user
+    print("Welcome!")
+    print(f"Hello {username}, Lets play a game.")
+username = input("Add username: ") # input for username
+greet_user(username)
+
+
+
+
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
